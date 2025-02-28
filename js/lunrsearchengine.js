72e83d53c948fe227db179f117f4a5e549f1d3c7
ChrisHammond <ChrisHammond@users.noreply.github.com>
2025-02-28 17:30:52 +0000

    deploy: 194cdbeec6fa62215725fd669eb1261910fba88e

--- js/lunrsearchengine.js
+++ js/lunrsearchengine.js
@@ -1573,7 +1573,7 @@ var documents = [{
     "id": 314,
     "url": "https://rainbowmarks.com/Events/2025/02/WickenheiserCup",
     "title": "2025 Wickenheiser Cup",
-    "body": "2025/02/28 - Photos from the 2025 Wickenheiser Cup where Rockwood Summit High School faced off against Parkway South High School at Centene Community Center. It was a close game until midway through the third, Parkway South was up 1-0 when Rockwood Summit was able to tie it up with a goal! After that however the wheels fell off the Rockwood Summit bus and Parkway South quickly put two more goals on the board to give themselves a lead and the momentum for the final few minutes of the game. Congrats to both Rockwood Summit and Parkway South for the great seasons! "
+    "body": "2025/02/28 - Photos from the 2025 Wickenheiser Cup where Rockwood Summit High School faced off against Parkway South High School at Centene Community Center. It was a close game until midway through the third, Parkway South was up 1-0 when Rockwood Summit was able to tie it up with a goal! After that however the wheels fell off the Rockwood Summit bus and Parkway South quickly put two more goals on the board to give themselves a lead and the momentum for the final few minutes of the game. Congrats to both Rockwood Summit and Parkway South for the great seasons! View 358 Photos from the Game "
     }, {
     "id": 315,
     "url": "https://rainbowmarks.com/Events/2025/01/StLouisHockey",
